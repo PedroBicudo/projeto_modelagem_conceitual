49fedab2650ae1a33d32c9f10747e865cc3f38f6
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-06-20 16:27:46 -0300

    chore: add dummy data when using dev profile

--- src/main/java/io/github/pedrobicudo/projeto_modelagem_conceitual/ProjetoModelagemConceitualApplication.java
+++ src/main/java/io/github/pedrobicudo/projeto_modelagem_conceitual/ProjetoModelagemConceitualApplication.java
@@ -1,13 +1,38 @@
 package io.github.pedrobicudo.projeto_modelagem_conceitual;
 
+import io.github.pedrobicudo.projeto_modelagem_conceitual.model.domain.entities.Category;
+import io.github.pedrobicudo.projeto_modelagem_conceitual.model.domain.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class ProjetoModelagemConceitualApplication {
+public class ProjetoModelagemConceitualApplication implements CommandLineRunner {
+
+	@Value("${spring.profiles.active}")
+	private String profiles;
+
+	@Autowired
+	private CategoryRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoModelagemConceitualApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (!profiles.equals("dev")) return;
+
+		List<Category> categories = List.of(
+			new Category(null, "Informática"),
+			new Category(null, "Escritório")
+		);
+
+		repository.saveAll(categories);
+
+	}
 }
